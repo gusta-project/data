96a9875c690b3f2f4a65bbdf3c44b7f1f98cc20c
ayan4m1 <andrew@bulletlogic.com>
2019-06-25 22:55:16 -0400

    replace pg with pg-promise

--- porter/src/database.js
+++ porter/src/database.js
@@ -1,7 +1,11 @@
-import { Pool } from 'pg';
+import pgRaw from 'pg-promise';
 
 import configs from './config';
 
+const pgPromise = pgRaw();
+
 const { database } = configs;
 
-export const pool = new Pool(database);
+export const { helpers } = pgPromise;
+
+export const db = pgPromise(database);
